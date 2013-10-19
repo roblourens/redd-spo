6aed5a4d1b415588b2744d0d80c8545c70a7625e
rob <roblourens@gmail.com>
2013-10-18 23:42:48 -0700

    Filter non-US songs

--- server/music/project.clj
+++ server/music/project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http.async.client "0.5.2"]
                  [org.clojure/data.json "0.2.3"]
-                 [clj-aws-s3 "0.3.7"]]
+                 [clj-aws-s3 "0.3.7"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main ^:skip-aot music.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- server/music/src/music/core.clj
+++ server/music/src/music/core.clj
@@ -3,9 +3,11 @@
   (:require [http.async.client :as http])
   (:require [clojure.data.json :as json])
   (:require [aws.sdk.s3 :as s3])
-  (:import [java.net URLEncoder]))
+  (:import [java.net URLEncoder])
+  (:use [clojure.contrib.core :only (-?>)]))
 
 (set! *warn-on-reflection* true)
+
 
 (def cred {:access-key "AKIAJKKRMAO6YBDXWITQ", :secret-key "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"})
 
@@ -13,26 +15,37 @@
 	(println "getting " url)
 	(with-open [client (http/create-client)]
 		(let [resp (http/GET client url)]
-			(-> resp http/await http/string (#(json/read-str % :key-fn keyword))))))
+			(-> resp http/await http/string (json/read-str :key-fn keyword)))))
 
 (defn map-vals [f m]
   (into {} (for [[k v] m] [k (f v)])))
 
 (defn search-spotify [q]
-	(defn first-href [d]
-		(-> d :tracks first :href))			
+	(defn filter-non-us [d]
+		(filter #(re-matches #".*US.*" (-> % :album :availability :territories)) d))
+
 	(defn clean-query [q]
-		""
+		"Add spaces around '-' and remove some unneeded things"
 		(-> q
-			(#(clojure.string/replace % #"-" " - "))
-			(#(clojure.string/replace % #"feat.|feat|ft.|ft|&amp;|:" ""))))
+			(clojure.string/replace #"-" " - ")
+			(clojure.string/replace #"feat.|feat|ft.|ft|&amp;|:" "")))
+
 	(defn do-search [q]
 		(println "searching for " q)
 		(-> (str "http://ws.spotify.com/search/1/track.json?q=" (URLEncoder/encode (clean-query q) "UTF-8"))
 			get-json
-			first-href))
-	(if-let [results do-search]
-		results))
+			:tracks
+			filter-non-us
+			first
+			:href))
+
+	(if-let [results (do-search q)]
+		results
+		(if-let [wo-brackets-q (clojure.core/re-matches #".*\[(.*)\]" q)]
+			(if (not (clojure.core/re-matches #"(?i) remix" (nth wo-brackets-q 1)))
+				(do-search (clojure.string/replace q #"\[.*\]" ""))
+				nil)
+			nil)))
 
 (defn get-submissions [name]
 	"Returns the submission title/urls for a subreddit, filtering self posts"
@@ -59,7 +72,7 @@
 (defn resolve-subreddit [name]
 	"Retrieves the subreddit submissions and resolves them to subreddit data ({:name altrock :tracks [...]})"
 	(println "resolve-subreddit " name)
-	{:name name
+	{:name (str "/r/" name)
 	 :tracks (map resolve-submission (get-submissions name))})
 
 (defn resolve-categories [category-map]
@@ -76,9 +89,7 @@
 	(let [results-json (json/write-str results)]
 		(s3/put-object cred "rl-reddspo" "results.json" results-json)
 		(s3/update-object-acl cred "rl-reddspo" "results.json" (s3/grant :all-users :read))
-		(spit "results.json" results-json))
-	)
-
+		(spit "results.json" results-json)))
 
 (defn -main [& args]
 	"Do the things."
