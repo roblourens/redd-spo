c0dcee68d105998ea3bf86d3f30adb82020ffe1b
Rob <roblourens@gmail.com>
2014-01-25 23:34:34 -0800

    old js

--- spotify/rl-reddit/js/config.js
+++
@@ -1,8 +0,0 @@
-Config = {};
-Mode = { Dev: "dev", Release: "rel"};
-
-Config.CurrentMode = Mode.Dev;
-Config.UrlBase = 
-    Config.CurrentMode == Mode.Dev ?
-        "http://rl-reddspo-dev.s3-website-us-east-1.amazonaws.com/" :
-        "http://rl-reddspo.s3-website-us-east-1.amazonaws.com/";
